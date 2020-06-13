f9491a1ac11ad86ffbd940c3c0d1a784bf5272e2
Marco Graf <github@marcograf.me>
2020-06-13 12:14:46 +0200

    update init

---
+++ sleekplan-wordpress.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Sleekplan for Wordpress
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.0
+ * Author:            Marco Graf
+ * Author URI:        https://marcograf.me/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       sleekplan-wp
+ * Domain Path:       /languages
+ */
